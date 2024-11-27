4ae1981a0677f5f35d1a209c9f95f3bb2afdc87c
tngh4037 <tngh4037@gmail.com>
2024-11-28 03:46:50 +0900

    study: 자동 구성 기반 예제

---
+++ src/main/java/hello/boot/config/MemoryConfig.java
@@ -0,0 +1,20 @@
+package hello.boot.config;
+
+import hello.memory.MemoryController;
+import hello.memory.MemoryFinder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MemoryConfig {
+
+    @Bean
+    public MemoryController memoryController() {
+        return new MemoryController(memoryFinder());
+    }
+
+    @Bean
+    public MemoryFinder memoryFinder() {
+        return new MemoryFinder();
+    }
+}
