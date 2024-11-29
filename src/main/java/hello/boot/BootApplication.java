05e48a9d1f4d79c92f3df0534b96d0d47edfb12f
tngh4037 <tngh4037@gmail.com>
2024-11-30 03:28:48 +0900

    study: 외부 설정 사용 - Environment

--- src/main/java/hello/boot/BootApplication.java
+++ src/main/java/hello/boot/BootApplication.java
@@ -1,9 +1,12 @@
 package hello.boot;
 
+import hello.boot.config.MyDataSourceEnvConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
-@SpringBootApplication
+@Import(MyDataSourceEnvConfig.class)
+@SpringBootApplication(scanBasePackages = "hello.boot.datasource")
 public class BootApplication {
 
 	public static void main(String[] args) {
